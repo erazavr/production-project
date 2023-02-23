7c08c94e08428b523a55378eeb45f8839ac02756
Ernur <erazavr85@gmail.com>
2023-02-23 15:10:51 +0600

    add redux store, add first entity

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,30 @@
+import { useTranslation } from 'react-i18next'
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue'
+import { useDispatch, useSelector } from 'react-redux'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { Button } from 'shared/ui/Button/Button'
+import { counterActions, counterSlice } from '../model/slice/counterSlice'
+
+interface CounterProps {
+  className?: string
+}
+
+export const Counter = ({ className }: CounterProps) => {
+  const { t } = useTranslation()
+  const counterValue = useSelector(getCounterValue)
+  const dispatch = useDispatch()
+  const increment = () => {
+    dispatch(counterActions.increment())
+  }
+  const decrement = () => {
+    dispatch(counterActions.decrement())
+  }
+
+  return (
+    <div className={classNames('', {}, [className])}>
+      <h1 data-testid='value-title'>{counterValue}</h1>
+      <Button data-testid='increment-btn' onClick={increment}>{t('increment')}</Button>
+      <Button data-testid='decrement-btn' onClick={decrement}>{t('decrement')}</Button>
+    </div>
+  )
+}
--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,3 +1,4 @@
+import { type StateSchema, StoreProvider } from 'app/providers/StoreProvider'
 import { MemoryRouter } from 'react-router-dom'
 import i18nForTests from 'shared/config/i18n/i18nForTests'
 import { render } from '@testing-library/react'
@@ -6,16 +7,19 @@ import { I18nextProvider } from 'react-i18next'
 
 interface componentRenderOptions {
   route?: string
+  initialState?: StateSchema
 }
 
 export function componentRender (component: ReactNode, options: componentRenderOptions = {}) {
-  const { route = '/' } = options
+  const { route = '/', initialState } = options
 
   return render(
-    <MemoryRouter initialEntries={[route]}>
-      <I18nextProvider i18n={i18nForTests}>
-        {component}
-      </I18nextProvider>
-    </MemoryRouter>
+    <StoreProvider initialState={initialState}>
+      <MemoryRouter initialEntries={[route]}>
+        <I18nextProvider i18n={i18nForTests}>
+          {component}
+        </I18nextProvider>
+      </MemoryRouter>
+    </StoreProvider>
   )
 }
