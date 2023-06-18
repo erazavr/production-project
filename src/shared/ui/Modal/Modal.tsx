8cb27bc63142fdb0e5bbaae9d113c7f568ef4db3
Ernur <erazavr85@gmail.com>
2023-06-18 14:25:36 +0600

    add useModal hook, upgrade drawer

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,12 +1,6 @@
-import React, {
-  type MutableRefObject,
-  type ReactNode,
-  useCallback,
-  useEffect,
-  useRef,
-  useState
-} from 'react'
+import React, { type ReactNode } from 'react'
 import { classNames, type Mods } from 'shared/lib/classNames/classNames'
+import { useModal } from 'shared/lib/hooks/useModal/useModal'
 import { Overlay } from '../Overlay/Overlay'
 import { Portal } from '../Portal/Portal'
 
@@ -25,42 +19,7 @@ const ANIMATION_DELAY = 200
 export const Modal = (props: ModalProps) => {
   const { className, children, isOpen, onClose, lazy } = props
 
-  const [isClosing, setIsClosing] = useState(false)
-  const [isMounted, setIsMounted] = useState(false)
-
-  const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>
-
-  const closeHandler = useCallback(() => {
-    if (!onClose) return
-    setIsClosing(true)
-    timerRef.current = setTimeout(() => {
-      onClose()
-      setIsClosing(false)
-    }, ANIMATION_DELAY)
-  }, [onClose])
-
-  const onKeyDown = useCallback((e: KeyboardEvent) => {
-    if (e.key === 'Escape') closeHandler()
-  }, [closeHandler])
-
-  useEffect(() => {
-    if (isOpen) {
-      setIsMounted(true)
-    }
-    return () => {
-      setIsMounted(false)
-    }
-  }, [isOpen])
-
-  useEffect(() => {
-    if (isOpen) {
-      window.addEventListener('keydown', onKeyDown)
-    }
-    return () => {
-      clearTimeout(timerRef.current)
-      window.removeEventListener('keydown', onKeyDown)
-    }
-  }, [isOpen, onKeyDown])
+  const { close, isClosing, isMounted } = useModal({ animationDelay: ANIMATION_DELAY, isOpen, onClose })
 
   const mods: Mods = {
     [cls.opened]: isOpen,
@@ -72,7 +31,7 @@ export const Modal = (props: ModalProps) => {
   return (
     <Portal>
       <div data-testid='modal' className={classNames(cls.Modal, mods, [className, 'app_modal'])}>
-        <Overlay onClick={closeHandler}/>
+        <Overlay onClick={close}/>
         <div className={cls.content}>
           {children}
         </div>
